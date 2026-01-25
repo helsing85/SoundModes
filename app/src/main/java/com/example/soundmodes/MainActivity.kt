69ace34431a672124d6e31165e00fc701570ec28
helsing85 <helsing85@gmail.com>
2026-01-25 22:02:06 +0100

    new Android Studio project, SDK=35

---
+++ app/src/main/java/com/example/soundmodes/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.soundmodes
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.soundmodes.ui.theme.SoundModesTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            SoundModesTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true, device = "id:pixel_8_pro", showSystemUi = true)
+@Composable
+fun GreetingPreview() {
+    SoundModesTheme {
+        Greeting("Android")
+    }
+}
