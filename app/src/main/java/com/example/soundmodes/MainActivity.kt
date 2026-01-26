1bc7d817c171588893e8b71cf2e293e56fd11c84
helsing <helsing85@gmail.com>
2026-01-26 19:39:18 +0100

    Adding topBar and two cards

--- app/src/main/java/com/example/soundmodes/MainActivity.kt
+++ app/src/main/java/com/example/soundmodes/MainActivity.kt
@@ -4,44 +4,102 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Card
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.soundmodes.ui.theme.SoundModesTheme
+
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            SoundModesTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            AppMainView()
         }
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun AppMainView() {
+    SoundModesTheme {
+        Scaffold(modifier = Modifier.fillMaxSize(),
+            topBar = {
+                AppBar()
+            }
+        ) { contentPadding ->
+            MainColumn(
+                name = "Android",
+                modifier = Modifier.padding(contentPadding)
+            )
+        }
+    }
 }
+
+
+@Composable
+private fun AppBar(
+    modifier: Modifier = Modifier
+) {
+    Row(
+        modifier = modifier
+            .fillMaxWidth()
+            .background(MaterialTheme.colorScheme.primary)
+            .padding(16.dp),
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically,
+    ) {
+        Text(
+            text = stringResource(R.string.app_name),
+            modifier = Modifier.padding(start = 16.dp),
+            color = MaterialTheme.colorScheme.onPrimary,
+            style = MaterialTheme.typography.headlineLarge,
+        )
+    }
+}
+
+
+@Composable
+fun MainColumn(name: String, modifier: Modifier = Modifier) {
+    Column(modifier = modifier) {
+        Card(modifier = Modifier
+            .fillMaxWidth()) {
+            Text(
+                text = "Hello $name!",
+                modifier = modifier
+            )
+        }
+        Card(modifier = Modifier
+            .fillMaxWidth()) {
+            Text(
+                text = "Hello $name!",
+                modifier = modifier
+            )
+        }
+    }
+}
+
 
 @Preview(showBackground = true, device = "id:pixel_8_pro", showSystemUi = true)
 @Composable
-fun GreetingPreview() {
-    SoundModesTheme {
-        Greeting("Android")
-    }
+fun MainAppPreview() {
+    AppMainView()
+
 }
