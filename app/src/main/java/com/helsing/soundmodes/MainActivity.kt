d6f517dd7e44fd35eedb0a7882cad85fec0cf4ce
helsing <helsing85@gmail.com>
2026-01-27 21:28:15 +0100

    Adding Cards with description and Notification Policy Access (Do not Disturb Access) button. Requesting permission.

--- app/src/main/java/com/helsing/soundmodes/MainActivity.kt
+++ app/src/main/java/com/helsing/soundmodes/MainActivity.kt
@@ -1,6 +1,10 @@
 package com.helsing.soundmodes
 
+import android.app.NotificationManager
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
+import android.provider.Settings
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -13,14 +17,21 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
 import androidx.compose.material3.Card
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -37,9 +48,17 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@OptIn(ExperimentalMaterial3Api::class)
+@Preview(
+    showBackground = true, device = "id:pixel_8_pro", showSystemUi = true
+)
 @Composable
-fun AppMainView() {
+fun MainAppPreview() {
+    AppMainView(true)
+
+}
+
+@Composable
+fun AppMainView(isPreview: Boolean = false) {
     SoundModesTheme {
         Scaffold(
             modifier = Modifier.fillMaxSize(),
@@ -48,7 +67,7 @@ fun AppMainView() {
             }
         ) { contentPadding ->
             MainColumn(
-                name = "Android",
+                isPreview = isPreview,
                 modifier = Modifier.padding(contentPadding)
             )
         }
@@ -79,7 +98,15 @@ private fun AppBar(
 
 
 @Composable
-fun MainColumn(name: String, modifier: Modifier = Modifier) {
+fun MainColumn(isPreview: Boolean, modifier: Modifier = Modifier) {
+    val context = LocalContext.current
+
+    var showDialog by remember { mutableStateOf(false) }
+
+    var isAccessibilityEnabled by remember {
+        mutableStateOf(isNotificationPolicyAccessPermissionEnabled(context, isPreview))
+    }
+
     Column(
         modifier = modifier
             .fillMaxSize()
@@ -92,82 +119,88 @@ fun MainColumn(name: String, modifier: Modifier = Modifier) {
             modifier = Modifier
                 .fillMaxWidth()
         ) {
-            Text(
-                text = "Hello $name!",
-                modifier = modifier
+            Column(
+                modifier = Modifier.padding(16.dp),
+                verticalArrangement = Arrangement.spacedBy(8.dp)
             )
+            {
+                Text(
+                    text = stringResource(R.string.description),
+                    style = MaterialTheme.typography.bodyLarge
+                )
+            }
         }
+
         Card(
-            modifier = Modifier
-                .fillMaxWidth()
+            modifier = Modifier.fillMaxWidth()
         ) {
-            Text(
-                text = "Hello $name!",
-                modifier = modifier
-            )
+            Column(
+                modifier = Modifier.padding(16.dp),
+                verticalArrangement = Arrangement.spacedBy(8.dp)
+            ) {
+                Text(
+                    stringResource(R.string.required_permission),
+                    style = MaterialTheme.typography.titleMedium
+                )
+
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.spacedBy(8.dp),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Text(
+                        stringResource(R.string.notification_policy_access),
+                        modifier = Modifier.weight(1f)
+                    )
+                    Button(
+                        onClick = { showDialog = true },
+                        enabled = !isAccessibilityEnabled,
+                    ) {
+                        Text(
+                            text = if (isAccessibilityEnabled) {
+                                stringResource(R.string.enabled)
+                            } else {
+                                stringResource(R.string.enable)
+                            }
+                        )
+                    }
+                }
+            }
         }
-        Card(
-            modifier = Modifier
-                .fillMaxWidth()
-        ) {
-            Text(
-                text = "Hello $name!",
-                modifier = modifier
-            )
-        }
-        Card(
-            modifier = Modifier
-                .fillMaxWidth()
-        ) {
-            Text(
-                text = "Hello $name!",
-                modifier = modifier
-            )
-        }
-        Card(
-            modifier = Modifier
-                .fillMaxWidth()
-        ) {
-            Text(
-                text = "Hello $name!",
-                modifier = modifier
-            )
-        }
-        Card(
-            modifier = Modifier
-                .fillMaxWidth()
-        ) {
-            Text(
-                text = "Hello $name!",
-                modifier = modifier
-            )
-        }
-        Card(
-            modifier = Modifier
-                .fillMaxWidth()
-        ) {
-            Text(
-                text = "Hello $name!",
-                modifier = modifier
-            )
-        }
-        Card(
-            modifier = Modifier
-                .fillMaxWidth()
-        ) {
-            Text(
-                text = "Hello $name!",
-                modifier = modifier
+
+        if (showDialog) {
+            AlertDialog(
+                onDismissRequest = { showDialog = false },
+                title = { Text(stringResource(R.string.notification_policy_access_permission_title)) },
+                text = { Text(stringResource(R.string.notification_policy_access_permission_description)) },
+                confirmButton = {
+                    TextButton(
+                        onClick = {
+                            showDialog = false
+                            context.startActivity(Intent(Settings.ACTION_NOTIFICATION_POLICY_ACCESS_SETTINGS))
+                        }
+                    ) {
+                        Text(stringResource(R.string.agree))
+                    }
+                },
+                dismissButton = {
+                    TextButton(
+                        onClick = { showDialog = false }
+                    ) {
+                        Text(stringResource(R.string.not_now))
+                    }
+                }
             )
         }
 
     }
 }
 
-
-@Preview(showBackground = true, device = "id:pixel_8_pro", showSystemUi = true)
-@Composable
-fun MainAppPreview() {
-    AppMainView()
-
+fun isNotificationPolicyAccessPermissionEnabled(context: Context, isPreview: Boolean): Boolean {
+    if (isPreview) {
+        return true
+    } else {
+        val nm = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        return nm.isNotificationPolicyAccessGranted()
+    }
 }
