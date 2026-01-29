0b118fcee1f1a34c73611e3a0e2d8f8c969fc5a8
helsing <helsing85@gmail.com>
2026-01-29 17:21:38 +0100

    Creating ZenRule for Silent Mode. Replacing icons.

--- app/src/main/java/com/helsing/soundmodes/SoundModesService.kt
+++ app/src/main/java/com/helsing/soundmodes/SoundModesService.kt
@@ -1,5 +1,6 @@
 package com.helsing.soundmodes
 
+import android.app.AutomaticZenRule
 import android.graphics.drawable.Icon
 import android.media.AudioManager
 import android.os.VibrationAttributes
@@ -7,6 +8,10 @@ import android.os.VibrationEffect
 import android.os.VibratorManager
 import android.service.quicksettings.Tile
 import android.service.quicksettings.TileService
+import android.app.NotificationManager
+import android.content.ComponentName
+import androidx.core.net.toUri
+import android.service.notification.Condition
 
 class SoundModesService : TileService() {
 
@@ -32,10 +37,67 @@ class SoundModesService : TileService() {
     private fun setIcon(): Int {
         val ringerMode = audioManager.ringerMode
         return when (ringerMode) {
-            0 -> R.drawable.volume_off_50dp
-            1 -> R.drawable.mobile_vibrate_50dp
-            else -> R.drawable.volume_up_50dp
+            0 -> R.drawable.volume_off
+            1 -> R.drawable.mobile_vibrate
+            else -> R.drawable.volume_up
         }
+    }
+
+    private fun getOrCreateZenRuleID(ruleName: String): String? {
+        val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+
+        // Check existing ZenRules
+        val existingRules = notificationManager.automaticZenRules
+        val existingId = existingRules.entries.firstOrNull { it.value.name == ruleName }?.key
+        if (existingId != null) return existingId
+
+
+        val iconResId = R.drawable.volume_off
+        val conditionUri = resources.getString(R.string.zen_rule_condition_uri).toUri()
+
+        // ComponentName (MainActivity is my starting class)
+        val configActivity = ComponentName(packageName, MainActivity::class.java.name)
+
+        // ZenRule builder (Android 10+)
+        val newRule = AutomaticZenRule.Builder(ruleName, conditionUri)
+            .setInterruptionFilter(NotificationManager.INTERRUPTION_FILTER_ALARMS)
+            // .setZenPolicy(zenPolicy)  // ZenPolicy is not needed when INTERRUPTION_FILTER_ALARMS
+            .setIconResId(iconResId)
+            .setTriggerDescription(resources.getString(R.string.silent_mode_description))
+            .setConfigurationActivity(configActivity)
+            .setEnabled(true)
+            .build()
+
+        // Register the rule and return ruleID
+        return notificationManager.addAutomaticZenRule(newRule)
+    }
+
+    fun toggleZenMode(ruleName: String, turnOn: Boolean) {
+        val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+
+        val ruleId = getOrCreateZenRuleID(ruleName) ?: return
+        val conditionUri = resources.getString(R.string.zen_rule_condition_uri).toUri()
+
+        val summary = resources.getString(
+            if (turnOn) R.string.silent_mode_active else R.string.silent_mode_inactive
+        )
+        val state = if (turnOn) Condition.STATE_TRUE else Condition.STATE_FALSE
+
+        // Before activation force deactivation
+        // If ZenRule is deactivated by switching to different Ringer Mode using other methods
+        // it cannot be reactivated again
+        if (turnOn) {
+            notificationManager.setAutomaticZenRuleState(
+                ruleId,
+                Condition(conditionUri, "", Condition.STATE_FALSE)
+            )
+        }
+
+        // Change state of the ZenRule
+        notificationManager.setAutomaticZenRuleState(
+            ruleId,
+            Condition(conditionUri, summary, state)
+        )
     }
 
     fun updateTile() {
@@ -69,11 +131,11 @@ class SoundModesService : TileService() {
             }
 
             if (currentMode == AudioManager.RINGER_MODE_SILENT) {
-                //toggleZenMode(getResources().getString(R.string.silent_mode), false)
+                toggleZenMode(resources.getString(R.string.silent_mode), false)
             }
 
             if (nextMode == AudioManager.RINGER_MODE_SILENT) {
-                //toggleZenMode(getResources().getString(R.string.silent_mode), true)
+                toggleZenMode(resources.getString(R.string.silent_mode), true)
             } else {
                 audioManager.setRingerMode(nextMode)
             }
