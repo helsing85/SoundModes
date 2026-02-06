059a917bb5da11e7612ea332bdb8f5787ff424bb
helsing <helsing85@gmail.com>
2026-02-06 17:16:56 +0100

    Changing version number (1.4)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.helsing.soundmodes"
         minSdk = 35
         targetSdk = 36
-        versionCode = 8
-        versionName = "1.4-beta"
+        versionCode = 9
+        versionName = "1.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
